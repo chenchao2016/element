fc13f684566b1bb16910213382e083184abe2d36
chenchao <chenchao@servyou.com.cn>
2017-09-20 19:46:43 +0800

    input button

--- examples/docs/zh-CN/input.md
+++ examples/docs/zh-CN/input.md
@@ -34,7 +34,8 @@
         state1: '',
         state2: '',
         state3: '',
-        state4: ''
+        state4: '',
+        state5:'',
       };
     },
     methods: {
@@ -426,8 +427,6 @@ export default {
       :fetch-suggestions="querySearch"
       placeholder="请输入内容"
       :trigger-on-focus="false"
-      :trigger-on-button="true" 
-       trigger-button="查询" trigger-button-class="mybutton" 
       @select="handleSelect"
     ></el-autocomplete>
   </el-col>
@@ -523,12 +522,61 @@ export default {
 
 ::: demo 可以通过按钮触发远程查询，并通过上下键选中下拉列表中的某个值。
 ```html
-<el-input-button
-  type="default">
-  hello
-</el-input-button>
-
+<el-row class="demo-inputbutton">
+  <el-col :span="12">
+    <div class="sub-title">输入后匹配输入建议</div>
+    <el-input-button
+      class="inline-input"
+      v-model="state5"
+      :fetch-suggestions="querySearch"
+      placeholder="请输入内容"
+      :trigger-on-button="true" 
+       trigger-button="查询" trigger-button-class="mybutton" 
+      @select="handleSelect"
+    ></el-input-button>
+  </el-col>
+</el-row>
 <script>
+  export default {
+    data() {
+      return {
+        restaurants: [],
+        state5: ''
+      };
+    },
+    methods: {
+      querySearch(queryString, cb) {
+        var restaurants = this.restaurants;
+        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      loadAll() {
+        return [
+          { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+          { "value": "三全", "address": "长宁区新渔路146号" },
+          { "value": "三明治", "address": "长宁区新渔路145号" },
+          { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+          { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
+          { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
+          { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
+          { "value": "贡茶", "address": "上海市长宁区金钟路633号" }
+        ];
+      },
+      handleSelect(item) {
+        console.log(item);
+      }
+    },
+    mounted() {
+      this.restaurants = this.loadAll();
+    }
+  }
+</script>
 ```
 :::
 
--- packages/input-button/src/input-button.vue
+++ packages/input-button/src/input-button.vue
@@ -1,16 +1,212 @@
 <template>
-  <div>hello <slot></slot></div>
+    <div class="el-input-button" v-clickoutside="close">
+        <el-input
+                ref="input"
+                v-bind="$props"
+                @compositionstart.native="handleComposition"
+                @compositionupdate.native="handleComposition"
+                @compositionend.native="handleComposition"
+                @change="handleChange"
+                @focus="handleFocus"
+                @keydown.up.native.prevent="highlight(highlightedIndex - 1)"
+                @keydown.down.native.prevent="highlight(highlightedIndex + 1)"
+                @keydown.enter.native="handleKeyEnter"
+                @keydown.native.tab="close"
+        >
+            <!--前置内容-->
+            <template slot="prepend" v-if="$slots.prepend">
+                <slot name="prepend"></slot>
+            </template>
+            <!--后置内容-->
+            <template slot="append" v-if="$slots.append">
+                <slot name="append"></slot>
+            </template>
+        </el-input>
+        <el-button v-if='triggerOnButton'  type="default" :class='triggerButtonClass' @click.native='handleclickOfmine' >{{triggerButton}}</el-button>
+        <el-inputbutton-suggestions
+                :props="props"
+                :class="[popperClass ? popperClass : '']"
+                ref="suggestions"
+                :suggestions="suggestions"
+        >
+        </el-inputbutton-suggestions>
+    </div>
 </template>
 <script>
-  import Emitter from 'element-ui/src/mixins/emitter';
+    import ElInput from 'element-ui/packages/input';
+    import ElButton from 'element-ui/packages/button';
+    import Clickoutside from 'element-ui/src/utils/clickoutside';
+    import ElInputbuttonSuggestions from './inputbutton-suggestions.vue';
+    import Emitter from 'element-ui/src/mixins/emitter';
 
-  export default {
-    name: 'ElInputButton',
-    mixins: [Emitter],
-    componentName:'ElInputButton',
+    export default {
+        name: 'ElInputButton',
 
-    props: {},
+        mixins: [Emitter],
 
-    methods: {}
-  };
+        componentName: 'ElInputButton',
+
+        components: {
+            ElInput,
+            ElButton,
+            ElInputbuttonSuggestions
+        },
+
+        directives: { Clickoutside },
+
+        props: {
+            props: {
+                type: Object,
+                default() {
+                    return {
+                        label: 'value',
+                        value: 'value'
+                    };
+                }
+            },
+            popperClass: String,
+            placeholder: String,
+            disabled: Boolean,
+            name: String,
+            size: String,
+            value: String,
+            autofocus: Boolean,
+            fetchSuggestions: Function,
+            triggerOnFocus: {
+                type: Boolean,
+                default: true
+            },
+            triggerOnButton: {
+                type: Boolean,
+                default: false
+            },
+            triggerButton: {
+                type: String,
+                default: '搜索'
+            },
+            triggerButtonClass: String,
+            customItem: String,
+            icon: String,
+            onIconClick: Function
+        },
+        data() {
+            return {
+                activated: false,
+                isOnComposition: false,
+                suggestions: [],
+                loading: false,
+                highlightedIndex: -1
+            };
+        },
+        computed: {
+            suggestionVisible() {
+                console.log('change computed!');
+                console.log('change computed!'+this.suggestions);
+                console.log('change computed!'+this.loading);
+                console.log('change computed!'+this.activated);
+                const suggestions = this.suggestions;
+                let isValidData = Array.isArray(suggestions) && suggestions.length > 0;
+                return (isValidData || this.loading) && this.activated;
+            }
+        },
+        watch: {
+            suggestionVisible(val) {
+                console.log('change watch!'+val);
+                this.broadcast('ElInputbuttonSuggestions', 'visible', [val, this.$refs.input.$refs.input.offsetWidth]);
+            }
+        },
+        methods: {
+
+            getData(queryString) {
+
+                if (this.triggerOnButton && !queryString) return;
+
+                this.loading = true;
+                this.fetchSuggestions(queryString, (suggestions) => {
+                    this.loading = false;
+                    if (Array.isArray(suggestions)) {
+                        this.suggestions = suggestions;
+                    } else {
+                        console.error('autocomplete suggestions must be an array');
+                    }
+                });
+            },
+            handleComposition(event) {
+                if (event.type === 'compositionend') {
+                    this.isOnComposition = false;
+                    this.handleChange(this.value);
+                } else {
+                    this.isOnComposition = true;
+                }
+            },
+            handleChange(value) {
+                this.$emit('input', value);
+                if (this.isOnComposition || (!this.triggerOnFocus && !value) || this.triggerOnButton) {
+                    this.suggestions = [];
+                    return;
+                }
+                console.log('listen input !');
+                this.getData(value);
+            },
+            handleFocus() {
+                this.activated = true;
+                if (this.triggerOnFocus && !this.triggerOnButton) {
+                    this.getData(this.value);
+                }
+            },
+            handleclickOfmine() {
+                this.getData(this.value);
+                this.$refs.input.$refs.input.focus();
+            },
+            close(e) {
+                this.activated = false;
+                if (this.triggerOnButton) this.suggestions = [];
+            },
+            handleKeyEnter(e) {
+                console.log('enter');
+                if (this.suggestionVisible && this.highlightedIndex >= 0 && this.highlightedIndex < this.suggestions.length) {
+                    e.preventDefault();
+                    this.select(this.suggestions[this.highlightedIndex]);
+                }
+            },
+            select(item) {
+                this.$emit('input', item[this.props.value]);
+                this.$emit('select', item);
+                this.$nextTick(_ => {
+                    this.suggestions = [];
+                });
+            },
+            highlight(index) {
+                console.log('highlight');
+                if (!this.suggestionVisible || this.loading) { return; }
+                if (index < 0) index = 0;
+                if (index >= this.suggestions.length) {
+                    index = this.suggestions.length - 1;
+                }
+                const suggestion = this.$refs.suggestions.$el.querySelector('.el-autocomplete-suggestion__wrap');
+                const suggestionList = suggestion.querySelectorAll('.el-autocomplete-suggestion__list li');
+
+                let highlightItem = suggestionList[index];
+                let scrollTop = suggestion.scrollTop;
+                let offsetTop = highlightItem.offsetTop;
+
+                if (offsetTop + highlightItem.scrollHeight > (scrollTop + suggestion.clientHeight)) {
+                    suggestion.scrollTop += highlightItem.scrollHeight;
+                }
+                if (offsetTop < scrollTop) {
+                    suggestion.scrollTop -= highlightItem.scrollHeight;
+                }
+
+                this.highlightedIndex = index;
+            }
+        },
+        mounted() {
+            this.$on('item-click', item => {
+                this.select(item);
+            });
+        },
+        beforeDestroy() {
+            this.$refs.suggestions.$destroy();
+        }
+    };
 </script>
